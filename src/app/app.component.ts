11115710bf0007b7802a025786d39146a9f2e5fa
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-02-22 00:43:48 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course-carsales-wilson-godoi';
+  title = 'Bem vindo ao Curso de Frontend com Angular';
 }
